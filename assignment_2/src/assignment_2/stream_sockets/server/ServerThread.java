cc582e1235dc5a444fe90ca299c9f467142bad21
Felix Lidö <felix.lido@gmail.com>
2024-01-11 13:54:35 +0100

    Serversocket assignment done

--- assignment_2/src/assignment_2/stream_sockets/server/ServerThread.java
+++ assignment_2/src/assignment_2/stream_sockets/server/ServerThread.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ServerThread extends Thread{
-    private final static char COMMAND_SYMBOL = '/';
     private final Socket client;
     private final PrintWriter out;
     private final BufferedReader in;
@@ -38,26 +36,26 @@ public class ServerThread extends Thread{
     }
 
     private void printClientInfo(Socket client) {
-        System.out.println(client.getInetAddress());
+        System.out.printf("Current clients: %d, Client connecting from: %s:%d\n",connectionCount(),client.getInetAddress(),client.getPort());
     }
 
     @Override
     public void run() {
         try {
             communicate(client);
-            System.out.printf("Client disconnected from: %s and port: %d\n", client.getInetAddress(), client.getPort());
+            System.out.printf("Client disconnected from: %s:%d\n",client.getInetAddress(), client.getPort());
         }catch (Exception e){
             System.out.println("Something went wrong in communication");
         }
 
         threadPool.remove(this);
-        System.out.println("Removed "  + this);
+        System.out.println("Removed " + this);
     }
 
     private void communicate(Socket clientSocket) throws IOException {
         while(!clientSocket.isClosed()){
             String message = this.in.readLine();
-            System.out.printf("<Client> %s\n",message);
+            System.out.printf("<%s:%d> %s\n",clientSocket.getInetAddress(),clientSocket.getPort(),message);
             if(message.equals("exit")){
                 clientSocket.close();
                 return;
